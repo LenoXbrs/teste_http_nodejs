903bc70243c5401e7e36eb009e97599c0ac064d7
Luis Eduardo <banedycraft@gmail.com>
2023-04-19 19:37:18 -0300

    pathParams e queryParams

--- index.js
+++ index.js
@@ -15,12 +15,34 @@ app.get("/blog",(req,res)=>{
     return res.send("<h1 style = 'color:red'> Esse é o meu blog<h1>")
     });
 
+    
+    //pathparam obrigatorio
     app.get("/ola/:nome",(req,res)=>{
         var {nome} = req.params;
        return res.send("Ola " + nome)
         });
-    
+    //path param opcional
+    app.get('/blogOp/:artigo?',(req,res)=>{
 
+        var artigo = req.params.artigo;
+
+        if(artigo){
+            return res.send("<h1 style = 'color:red'> Esse é o meu blog opcional<h1>")
+        } else{
+            return res.send("<h1 style = 'color:red'> Esse é o meu blog nao opcional<h1>")
+        }
+    })
+    //query param 
+
+    app.get('/canal/youtube', (req,res)=>{
+        var canal = req.query["canal"];
+        if(canal){
+            res.send("Canal " + canal + " encontrado!")
+        } else{
+            res.json({"error" : "variavel canal é necessaria"})
+        }
+    })
+        
 app.listen(3000,function(erro){
     if(erro){
         console.log("Ocorreu um erro");
